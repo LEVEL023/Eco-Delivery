087f8d15900c1b42d5e6cebeebb72e417953a07b
Shize Ligit config --global user.name shize.li@sjsu.edu <54549527+lishize1234@users.noreply.github.com>
2021-05-30 23:10:51 -0700

    Stepper update

---
+++ Frontend/delivery-management-frontend/src/components/App.js
@@ -0,0 +1,13 @@
+import logo from '../logo.svg';
+import FillAddress from './FillAddress.js';
+
+function App() {
+
+  return (
+    <div className="App">
+      <FillAddress />
+    </div>
+  );
+}
+
+export default App;
