80bc99c742cab9b1815cbb04eec878ac1d9e11a5
xiaoma <helloxiaomaa@outlook.com>
2021-05-31 14:44:01 +0800

    first git commit for eco delivery project

---
+++ Frontend/delivery-management-frontend/src/components/AddressForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class AddressForm extends React.Component {
+    render = () => {
+        return (
+            null
+        )
+    }
+}
+
+export default AddressForm;
